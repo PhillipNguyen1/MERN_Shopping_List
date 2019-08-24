f33ca0f4993c129175a876c8a21fea0f9e9136c1
Phillip Nguyen <phillipnguyen0421@gmail.com>
2019-08-24 18:32:22 -0500

    post working for api/users

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const path = require("path");
 
 // file that contain all calls to items api
-const items = require("./routes/api/items");
 
 const app = express();
 
 // BodyParser Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
 
 // Connect to Mongo
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useCreateIndex: true })
   .then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err));
 
 // Use Routes
-app.use("/api/items", items);
+app.use("/api/items", require("./routes/api/items"));
+app.use("/api/users", require("./routes/api/users"));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
