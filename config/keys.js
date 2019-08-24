5a363452ae865b166251bc96d6460c01184a76de
Phillip Nguyen <phillipnguyen0421@gmail.com>
2019-08-24 14:34:57 -0500

    Added Mongodb uri

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI:
-    "mongodb://ptnguy44:Ilikecheese1@firstproject-oxfix.mongodb.net/mern_shopping?retryWrites=true&w=majority"
+    "mongodb+srv://ptnguy44:Ilikecheese1@firstproject-oxfix.mongodb.net/mern_shopping?retryWrites=true&w=majority"
 };
