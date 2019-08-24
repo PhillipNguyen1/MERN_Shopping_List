2e80bb2a56729681c28ef02f40276c4aec6adc0e
Phillip Nguyen <phillipnguyen0421@gmail.com>
2019-08-24 11:55:28 -0500

    initial commit. Shopping list server created

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://ptnguy44:Ilikecheese1@firstproject-oxfix.mongodb.net/mern_shopping?retryWrites=true&w=majority"
+};
---
+++ routes/api/items.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const router = express.Router();
+
+// Item Model
+const Item = require("../../models/Item");
+
+// @route   GET api/items
+// @desc    GET All items
+// @access  Public
+router.get("/", (req, res) => {
+  Item.find()
+    .sort({ date: -1 })
+    .then(items => res.json(items));
+});
+
+// @route   POST api/items
+// @desc    Create a item
+// @access  Public
+router.post("/", (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+
+  newItem.save().then(item => res.json(newItem));
+});
+
+// @route   DELETE api/items/:id
+// @desc    Delete an item by id
+// @access  Public
+router.delete("/:id", (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({ success: true })))
+    .catch(() => res.status(404).json({ success: false }));
+});
+
+// TODO: fix PATCH
+
+// @route   PATCH api/items/:id
+// @desc    Update an item by id
+// @access  Public
+router.patch("/:id", (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => {
+      const newItem = new Item({
+        name: req.body.name
+      });
+
+      item = newItem;
+      // item.patch().then(() => res.json({ success: true }));      
+    })
+    .catch(() => res.status(404).json({ success: false }));
+});
+
+module.exports = router;
