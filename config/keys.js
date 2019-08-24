18d67ebe44a4ded7afe65d075a31289474e1d987
Phillip Nguyen <phillipnguyen0421@gmail.com>
2019-08-24 14:25:51 -0500

    make it better

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://ptnguy44:Ilikecheese1@firstproject-oxfix.mongodb.net/mern_shopping?retryWrites=true&w=majority"
+    "mongodb://ptnguy44:Ilikecheese1@firstproject-oxfix.mongodb.net/mern_shopping?retryWrites=true&w=majority"
 };
